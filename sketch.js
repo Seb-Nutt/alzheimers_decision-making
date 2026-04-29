7e3736d57b865664f9f0c92a059c8a0d26f5d066
Seb-Nutt <nuttsebastian@gmail.com>
2026-04-29 15:25:20 -0600

    Worked on the brain model

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ class BrainNode{
   }
 
   connect(otherNode){
-    if (dist(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z) < 100){
+    if (dist(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z) < 200){
       fill(255);
       line(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z);
     }
@@ -47,8 +47,8 @@ function setup() {
   for (node of nodePositions){
     brainNodes.push(new BrainNode(node[0],node[1],node[2]));
 
-    //if the position is marked with a -1 at the end of it, then mirror the node
-    if (node[node.length - 1] === MIRROR_NODE){
+    //Mirror the 
+    if (node[2] !== MIRROR_NODE){
       brainNodes.push(new BrainNode(node[0],node[1],node[2]*-1));
     }
   }
@@ -63,7 +63,6 @@ function draw() {
 function drawBrain(){
   orbitControl();
   stroke(255);
-
   for (node of brainNodes){
     for (otherNode of brainNodes){
       if (node !== otherNode){
