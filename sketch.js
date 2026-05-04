4dce357f3b0dd5762b311c427feaab5b37585a32
Seb-Nutt <nuttsebastian@gmail.com>
2026-05-04 15:26:03 -0600

    Finalised the brain mde, added a system to stop rotation, began work on the regions of the brain

--- sketch.js
+++ sketch.js
@@ -6,29 +6,49 @@
 // - describe what you did to take this project "above and beyond"
 
 let brainNodes = [];
+let brainRegions = [];
 let nodePositions;
 let rotationAngle = 0;
-const MIRROR_NODE = -1;
+let selectingLobe = false;
 
 class BrainNode{
   constructor(x,y,z) {
     this.x = x;
     this.y = y;
     this.z = z;
+    this.NODE_CONNECTION_RANGE = 200;
   }
 
   connect(otherNode){
-    if (dist(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z) < 200){
+    if (dist(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z) < this.NODE_CONNECTION_RANGE){
       fill(255);
       line(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z);
     }
   }
 }
 
+class BrainRegion{
+  constructor(borderNodes,r,g,b) {
+    this.borderNodes = borderNodes;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.alpha = 100;
+  }
+
+  colorIn(){
+    fill(this.r,this.g,this.b,this.alpha);
+    beginShape();
+    for (let point of borderNodes){
+      vertex(point[0],point[1]);
+    }
+    endShape(CLOSE);
+  }
+}
+
+
 function preload() {
   nodePositions = loadStrings("assets/nodePositions.txt");
-
-  
 }
 
 
@@ -46,13 +66,19 @@ function setup() {
 
 
   for (node of nodePositions){
-    brainNodes.push(new BrainNode(node[0],node[1],node[2]));
+    //create the regions
+    //add a number on each node to classify the node in a region
 
-    //Mirror the 
-    if (node[2] !== MIRROR_NODE){
-      brainNodes.push(new BrainNode(node[0],node[1],node[2]*-1));
+    //create the nodes
+    brainNodes.push(new BrainNode(node[0]-150,node[1]+50,node[2]));
+
+    //Mirror the node if it is not centered
+    if (node[2] !== 0){
+      brainNodes.push(new BrainNode(node[0]-150,node[1]+50,node[2]*-1));
     }
   }
+
+  
 }
 
 function draw() { 
@@ -62,9 +88,19 @@ function draw() {
 
 
 function drawBrain(){
-  if (mouseIsPressed){
-    orbitControl();
-    rotationAngle = 0;
+
+  //rotate the brain if a lobe is not being selected
+  if (selectingLobe){
+    //rotate the brain back into position
+    if (rotationAngle - 0.1 > 0){
+      rotationAngle -= 0.05;
+    }
+
+    for (let region of brainRegions){
+
+    }
+
+    rotateY(rotationAngle);
   }
   else{
     rotationAngle+= 0.002;
@@ -80,5 +116,11 @@ function drawBrain(){
     }
   }
 
-  //rotate the brain
+
+}
+
+function keyPressed(){
+  if (key === ' '){
+    selectingLobe = !selectingLobe;
+  }
 }
