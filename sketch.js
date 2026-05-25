fefad5455384a362e06d8c17abf793fe3b7856f8
Seb-Nutt <nuttsebastian@gmail.com>
2026-05-25 15:25:59 -0600

    Finished the parent button class, began work on the simulation buttons

--- sketch.js
+++ sketch.js
@@ -25,6 +25,12 @@ const PARIETAL = 3;
 const FRONTAL = 4;
 const TEMPORAL = 5;
 let regionWriteUps;
+let parentSimulationButton;
+let simulationButtons = [];
+const HOMEPAGE = 0;
+const IOWA_GAMBLING_TASK = 1;
+const PRISONERS_DILEMMA = 2;
+let workspace = HOMEPAGE;
 
 class BrainNode{
   constructor(x,y,z,region) {
@@ -50,6 +56,7 @@ class Button{
     this.y = y;
     this.buttonText = buttonText;
     this.buttonColor = buttonColor;
+    this.backgroundColor = color(red(this.buttonColor)-70,green(this.buttonColor)-70,blue(this.buttonColor)-70);
     this.hoveredColor = color(red(this.buttonColor)-100,green(this.buttonColor)-100,blue(this.buttonColor)-100);
     this.buttonWidth = this.buttonText.length*20;
     this.buttonHeight = 100;
@@ -61,77 +68,68 @@ class Button{
   drawButton(opacity){
     stroke('white');
 
-    
-    fill(this.detectHovering() ? this.buttonColor : this.hoveredColor);
-
+    fill(this.detectHovering() ? this.hoveredColor : this.backgroundColor,opacity);
+    rotateY(0);
     rect(this.x,this.y,this.buttonWidth,this.buttonHeight);
     textAlign(CENTER);
     textSize(this.titleSize);
     textFont(quicksandFont);
-    fill(this.primaryColor,opacity);
+    fill(this.buttonColor,opacity);
     text(this.buttonText, this.x + this.buttonWidth/2, this.y + this.buttonHeight/2);
 
     if (this.clicked){
-      this.displayInfoChunk();
+      this.triggerEffect();
     }
   }
 
   detectHovering(){
     return this.x < mouseX-width/2 && this.y < mouseY-height/2 && this.x + this.buttonWidth > mouseX-width/2 && this.y + this.buttonHeight > mouseY- height/2;
   }
+
 }
 
 class RegionButton extends Button{
-  constructor(x,y, buttonText, buttonColor, hoveredColor, region,id){
-    super(x,y, buttonText, buttonColor, hoveredColor)
-    this.region = region;
+  constructor(x,y, buttonText, buttonColor, id, hoveredColor, buttonWidth, buttonHeight, clicked){
+    super(x,y, buttonText, buttonColor, hoveredColor, buttonWidth, clicked, buttonHeight);
+    this.region = buttonText;
     this.regionID = id;
-    this.primaryColor = regionColors[this.regionID];
-    this.secondaryColor;
-    this.buttonWidth = this.region.length*20;
+    this.buttonWidth = this.buttonText.length*20;
     this.buttonHeight = 100;
     this.backgroundColorDeficit = 50;
-    this.clicked = false;
     this.titleSize = this.buttonHeight/3;
   }
 
-  drawButton(opacity){
-    stroke('white');
-
-    this.backgroundColorDeficit = this.detectHovering() ? 100 : 70;
-    
-    this.secondaryColor = color(red(this.primaryColor)-100,green(this.primaryColor)-100,blue(this.primaryColor)-100);
-
-    fill(color(red(this.primaryColor)-this.backgroundColorDeficit,green(this.primaryColor)-this.backgroundColorDeficit,blue(this.primaryColor)-this.backgroundColorDeficit));
-    rect(this.x,this.y,this.buttonWidth,this.buttonHeight);
-    textAlign(CENTER);
-    textSize(this.titleSize);
-    textFont(quicksandFont);
-    fill(this.primaryColor,opacity);
-    text(this.region, this.x + this.buttonWidth/2, this.y + this.buttonHeight/2);
-
-    if (this.clicked){
-      this.displayInfoChunk();
-    }
-  }
-
-  displayInfoChunk(){
+  triggerEffect(){
     //textbox
-    orbitControl();
     translate(0,0,0);
-    fill(this.secondaryColor);
-    stroke(this.primaryColor);
+    fill(this.backgroundColor);
+    stroke(this.buttonColor);
     box(width/2,height/2);
 
     //text
-    
     push();
     translate(0,0,250);
-    fill(this.primaryColor);
+    textAlign(CENTER);
+    fill(this.buttonColor);
+    textFont(quicksandFont);
+    textSize(this.titleSize);
     text(this.region,0,-height/4 + this.titleSize);
     textSize(this.titleSize/1.5);
     text(regionWriteUps[this.regionID],-width/4,-height/8,width/2);
     pop();
+  }
+}
+
+class SimulationButton extends Button{
+  constructor(x, y, buttonText, buttonColor, id, hoveredColor, buttonWidth, buttonHeight, clicked){
+    super(x, y, buttonText, buttonColor, hoveredColor, buttonWidth, buttonHeight, clicked);
+    this.id = id;
+  }
+
+  triggerEffect(){
+    if (this.id !== 'parent'){
+      workspace = this.id;
+    }
   }
 }
 
@@ -156,13 +154,18 @@ function setup() {
     }
   }
 
+  //create the parent simulation button
+  parentSimulationButton = new SimulationButton(-19*width/39, -5*height/11,'Simulations',color(200),'parent');
 
-  regionButtons.push(new RegionButton(60,350,'Brain Stem',0));
-  regionButtons.push(new RegionButton(-578, 91, 'Cerebellum',1));
-  regionButtons.push(new RegionButton(-665, -187, 'Occipital Lobe',2));
-  regionButtons.push(new RegionButton(-220, -450, 'Parietal Lobe',3));
-  regionButtons.push(new RegionButton(420, -355, 'Frontal Lobe',4));
-  regionButtons.push(new RegionButton(130, 160, 'Temporal Lobe', 5));
+  simulationButtons.push(new SimulationButton(parentSimulationButton.x, parentSimulationButton.y + parentSimulationButton.buttonHeight*2, 'Iowa Gambling Task', color(150), IOWA_GAMBLING_TASK));
+
+  //create the buttons for each region of the brain
+  regionButtons.push(new RegionButton(60,350,'Brain Stem', regionColors[BRAIN_STEM],BRAIN_STEM));
+  regionButtons.push(new RegionButton(-578, 91, 'Cerebellum', regionColors[CEREBELLUM],CEREBELLUM));
+  regionButtons.push(new RegionButton(-665, -187, 'Occipital Lobe', regionColors[OCCIPITAL],OCCIPITAL));
+  regionButtons.push(new RegionButton(-220, -450, 'Parietal Lobe',regionColors[PARIETAL],PARIETAL));
+  regionButtons.push(new RegionButton(420, -355, 'Frontal Lobe', regionColors[FRONTAL],FRONTAL));
+  regionButtons.push(new RegionButton(130, 160, 'Temporal Lobe', regionColors[TEMPORAL],TEMPORAL));
 
   for (node of nodePositions){
     //create the nodes and classify their region
@@ -179,7 +182,10 @@ function setup() {
 
 function draw() { 
   background(0);
-  drawBrain();
+  if (workspace === HOMEPAGE){
+    drawBrain();
+    drawSimulationButtons();
+  }
 }
 
 
@@ -225,20 +231,9 @@ function drawBrain(){
       if (brainRotationAngle === 0 && buttonOpacity <= 100){
         buttonOpacity += 0.05;
       }
-      // just make a button next to each section
-      // for (let region = 0; region < regionNodes.length; region++){
-      //   fill(regionColors[region],50);
-      //   stroke('white');
-      //   beginShape();
-      //   for (let node of regionBorderNodes[region]){
-      //     vertex(node[0],node[1],node[2]);
-      //   }
-      //   endShape(CLOSE);
-      // }
 
       for (let button of regionButtons){
         button.drawButton(buttonOpacity);
-        // console.log(button.detectHovering());
       }
 
     }
@@ -256,15 +251,41 @@ function keyPressed(){
 }
 
 function mouseClicked(){
+  console.log(mouseX,mouseY);
   if (selectingLobe){
     for (let button of regionButtons){
       if (button.detectHovering()){
         button.clicked = !button.clicked;
-        button.displayInfoChunk();
+        button.triggerEffect();
       }
       else{
         button.clicked = false;
       }
     }
+    if (parentSimulationButton.detectHovering()){
+      parentSimulationButton.clicked = true;
+      parentSimulationButton.triggerEffect();
+    }
+    else{
+      parentSimulationButton.clicked = false;
+    }
+
+    // for (let childButton of simulationButtons){
+    //   if (childButton.detectHovering){
+    //     childButton.triggerEffect();
+    //   }
+    // }
   }
+}
+
+function drawSimulationButtons(){
+  push();
+  rotateY(0);
+  parentSimulationButton.drawButton();
+  if (parentSimulationButton.clicked){
+    for (let childButton of simulationButtons){
+      childButton.drawButton();
+    }
+  }
+  pop();
 }
